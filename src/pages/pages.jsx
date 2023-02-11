f009ce9aac8b49577d01d602fe7043eb14e5b314
NdekoCode <arickbulakali@gmail.com>
2023-02-11 13:15:22 +0200

    refactor: add [artials in components and pages structures

---
+++ src/pages/pages.jsx
@@ -0,0 +1,16 @@
+export { default as Area } from "./Charts/Area";
+export { default as Bar } from "./Charts/Bar";
+export { default as ColorMapping } from "./Charts/ColorMapping";
+export { default as Financial } from "./Charts/Financial";
+export { default as Line } from "./Charts/Line";
+export { default as Pie } from "./Charts/Pie";
+export { default as Pyramid } from "./Charts/Pyramid";
+export { default as Stacked } from "./Charts/Stacked";
+export { default as Calendar } from "./Partials/Calendar";
+export { default as ColorPicker } from "./Partials/ColorPicker";
+export { default as Customers } from "./Partials/Customers";
+export { default as Ecommerce } from "./Partials/Ecommerce";
+export { default as Editor } from "./Partials/Editor";
+export { default as Employees } from "./Partials/Employees";
+export { default as Kanban } from "./Partials/Kanban";
+export { default as Orders } from "./Partials/Orders";
