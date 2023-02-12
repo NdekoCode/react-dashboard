72d6958d4f913c217532532d52d97420511f60d1
NdekoCode <arickbulakali@gmail.com>
2023-02-12 23:19:03 +0200

    feat: add navbar button toggle

--- src/App.jsx
+++ src/App.jsx
@@ -30,10 +30,15 @@ function App() {
             <div className="w-72 sidebar fixed dark:bg-secondary-dark-bg bg-white">
               <Sidebar />
             </div>
-          ) : null}
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
           <div
-            className={`"dark:bg-main-bg bg-main-bg min-h-screen  w-full"
-              ${activeMenu ? "md:ml-72" : "flex-2"}`}
+            className={`dark:bg-main-bg bg-main-bg min-h-screen  w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
@@ -44,7 +49,6 @@ function App() {
               {routes.map(({ path, component }, index) => (
                 <Route key={index} path={path} element={component} />
               ))}
-              <Route path="/" element="E-commerce" />
             </Routes>
           </div>
         </div>
