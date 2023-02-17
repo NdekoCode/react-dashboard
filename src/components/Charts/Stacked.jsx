96e93e0c488392c7cf30eaa9cf5d207a2333678b
NdekoCode <arickbulakali@gmail.com>
2023-02-17 19:13:29 +0200

    feat: add chart component

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -3,11 +3,14 @@ import {
   ChartComponent,
   Inject,
   Legend,
+  SeriesCollectionDirective,
+  SeriesDirective,
   StackingColumnSeries,
   Tooltip,
 } from "@syncfusion/ej2-react-charts";
 import React from "react";
 import {
+  stackedCustomSeries,
   stackedPrimaryXAxis,
   stackedPrimaryYAxis,
 } from "../../libs/data/dummy";
@@ -21,8 +24,24 @@ const Stacked = ({ width, height }) => {
         id="stark chart"
         primaryXAxis={stackedPrimaryXAxis}
         primaryYAxis={stackedPrimaryYAxis}
+        chartArea={{
+          border: {
+            width: 0,
+          },
+        }}
+        tooltip={{
+          enable: true,
+        }}
+        legendSettings={{
+          background: "white",
+        }}
       >
         <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+        <SeriesCollectionDirective>
+          {stackedCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
       </ChartComponent>
     </div>
   );
