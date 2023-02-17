56844030407526f0212c5f4f8496712286e4d4ff
NdekoCode <arickbulakali@gmail.com>
2023-02-17 19:27:52 +0200

    refactor: add description on hovert chart component

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -21,7 +21,7 @@ const Stacked = ({ width, height }) => {
       <ChartComponent
         width={width}
         height={height}
-        id="stark chart"
+        id="charts"
         primaryXAxis={stackedPrimaryXAxis}
         primaryYAxis={stackedPrimaryYAxis}
         chartArea={{
