947d1810fb90d2fdd7d3581cd6b8acd8eb0c269e
NdekoCode <arickbulakali@gmail.com>
2023-02-18 21:39:21 +0200

    feat: add kanban component

--- src/pages/Partials/Kanban.jsx
+++ src/pages/Partials/Kanban.jsx
@@ -1,10 +1,20 @@
+import { ColumnsDirective } from "@syncfusion/ej2-react-grids";
+import { ColumnDirective, KanbanComponent } from "@syncfusion/ej2-react-kanban";
 import React from "react";
 import { Header } from "../../components/components";
+import { kanbanGrid } from "../../libs/data/dummy";
 
 function Kanban() {
   return (
     <div className="FadeAp FadeAp m-2 p-2 md:m-10 md:p-10 mt-20 bg-white rounded-3xl">
       <Header category="App" title="Kanban" />
+      <KanbanComponent>
+        <ColumnsDirective>
+          {kanbanGrid.map((kanbanItem, index) => (
+            <ColumnDirective key={index} {...kanbanItem} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
     </div>
   );
 }
