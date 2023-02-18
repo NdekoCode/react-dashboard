e52d06ae9e770a0cf5ca5ac815fc061cc86c8027
NdekoCode <arickbulakali@gmail.com>
2023-02-18 20:41:38 +0200

    feat: add data interactive in calendar component

--- src/pages/Partials/Calendar.jsx
+++ src/pages/Partials/Calendar.jsx
@@ -10,10 +10,17 @@ import {
   WorkWeek,
 } from "@syncfusion/ej2-react-schedule";
 import React from "react";
+import { scheduleData } from "../../libs/data/dummy";
 function Calendar() {
   return (
     <div className="m-2 p-2 md:m-10 md:p-10 mt-20 bg-white rounded-3xl">
-      <ScheduleComponent>
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{
+          dataSource: scheduleData,
+        }}
+        selectedDate={new Date(2021, 0, 10)}
+      >
         <Inject
           services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
         />
