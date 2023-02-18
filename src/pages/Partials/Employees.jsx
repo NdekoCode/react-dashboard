144b539716044f3b87eed795c064eeae834210a2
NdekoCode <arickbulakali@gmail.com>
2023-02-18 17:57:11 +0200

    feat: add search by name component

--- src/pages/Partials/Employees.jsx
+++ src/pages/Partials/Employees.jsx
@@ -2,6 +2,10 @@ import {
   ColumnDirective,
   ColumnsDirective,
   GridComponent,
+  Inject,
+  Page,
+  Search,
+  Toolbar,
 } from "@syncfusion/ej2-react-grids";
 import React from "react";
 import { Header } from "../../components/components";
@@ -12,16 +16,18 @@ function Employees() {
     <div className="m-2 p-2 md:m-10 md:p-10 rounded-3xl bg-white">
       <Header category="Page" title="Employees" />
       <GridComponent
-        id="gridcomp"
         dataSource={employeesData}
         allowPaging
         allowSorting
+        toolbar={["Search"]}
+        width="auto"
       >
         <ColumnsDirective>
           {employeesGrid.map((employee, index) => (
             <ColumnDirective key={index} {...employee} />
           ))}
         </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
       </GridComponent>
     </div>
   );
