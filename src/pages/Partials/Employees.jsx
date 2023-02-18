a2b090e427056254afc51331fb37bc20e69b2ab9
NdekoCode <arickbulakali@gmail.com>
2023-02-18 17:51:18 +0200

    feat: display employees data

--- src/pages/Partials/Employees.jsx
+++ src/pages/Partials/Employees.jsx
@@ -5,13 +5,18 @@ import {
 } from "@syncfusion/ej2-react-grids";
 import React from "react";
 import { Header } from "../../components/components";
-import { employeesGrid } from "../../libs/data/dummy";
+import { employeesData, employeesGrid } from "../../libs/data/dummy";
 
 function Employees() {
   return (
     <div className="m-2 p-2 md:m-10 md:p-10 rounded-3xl bg-white">
       <Header category="Page" title="Employees" />
-      <GridComponent id="gridcomp">
+      <GridComponent
+        id="gridcomp"
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+      >
         <ColumnsDirective>
           {employeesGrid.map((employee, index) => (
             <ColumnDirective key={index} {...employee} />
