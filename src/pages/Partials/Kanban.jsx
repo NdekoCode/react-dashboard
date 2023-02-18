f7ce60257a82cbc767bea6e3615f448b2a6e466d
NdekoCode <arickbulakali@gmail.com>
2023-02-18 21:44:17 +0200

    feat: kanban interactive with dragandDrop

--- src/pages/Partials/Kanban.jsx
+++ src/pages/Partials/Kanban.jsx
@@ -2,13 +2,18 @@ import { ColumnsDirective } from "@syncfusion/ej2-react-grids";
 import { ColumnDirective, KanbanComponent } from "@syncfusion/ej2-react-kanban";
 import React from "react";
 import { Header } from "../../components/components";
-import { kanbanGrid } from "../../libs/data/dummy";
+import { kanbanData, kanbanGrid } from "../../libs/data/dummy";
 
 function Kanban() {
   return (
     <div className="FadeAp FadeAp m-2 p-2 md:m-10 md:p-10 mt-20 bg-white rounded-3xl">
       <Header category="App" title="Kanban" />
-      <KanbanComponent>
+      <KanbanComponent
+        id="kanban"
+        dataSource={kanbanData}
+        cardSettings={{ contentField: "Summary", headerField: "Id" }}
+        keyField="Status"
+      >
         <ColumnsDirective>
           {kanbanGrid.map((kanbanItem, index) => (
             <ColumnDirective key={index} {...kanbanItem} />
