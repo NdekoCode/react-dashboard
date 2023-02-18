8cc67cafe3a8e3e592d46d144ae009d3d06d83e0
NdekoCode <arickbulakali@gmail.com>
2023-02-18 17:49:27 +0200

    style: add employees header table

--- src/pages/Partials/Employees.jsx
+++ src/pages/Partials/Employees.jsx
@@ -1,7 +1,25 @@
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  GridComponent,
+} from "@syncfusion/ej2-react-grids";
 import React from "react";
+import { Header } from "../../components/components";
+import { employeesGrid } from "../../libs/data/dummy";
 
 function Employees() {
-  return <div>Employees</div>;
+  return (
+    <div className="m-2 p-2 md:m-10 md:p-10 rounded-3xl bg-white">
+      <Header category="Page" title="Employees" />
+      <GridComponent id="gridcomp">
+        <ColumnsDirective>
+          {employeesGrid.map((employee, index) => (
+            <ColumnDirective key={index} {...employee} />
+          ))}
+        </ColumnsDirective>
+      </GridComponent>
+    </div>
+  );
 }
 
 export default Employees;
