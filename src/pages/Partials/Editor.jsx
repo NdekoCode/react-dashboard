65c78b656106ab35b2b078f430853f7089514cfe
NdekoCode <arickbulakali@gmail.com>
2023-02-18 22:41:52 +0200

    feat: add html editor to the dashboard

--- src/pages/Partials/Editor.jsx
+++ src/pages/Partials/Editor.jsx
@@ -1,7 +1,24 @@
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
 import React from "react";
+import { Header } from "../../components/components";
 
 function Editor() {
-  return <div className="FadeAp">Editor</div>;
+  return (
+    <div className="FadeAp m-2 p-2 md:m-10 md:p-10 rounded-3xl bg-white">
+      <Header category="App" title="Editor" />
+      <RichTextEditorComponent>
+        <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
+  );
 }
 
 export default Editor;
