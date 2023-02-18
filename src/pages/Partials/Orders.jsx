d3d0c90eaa9942b75aa2eef2a64cef6e9414050f
NdekoCode <arickbulakali@gmail.com>
2023-02-18 10:59:17 +0200

    feat: add simple header for order page

--- src/pages/Partials/Orders.jsx
+++ src/pages/Partials/Orders.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Header } from "../../components/components";
 
-function Orders() {
-  return <div>Orders</div>;
-}
+const Orders = () => {
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+    </div>
+  );
+};
 
 export default Orders;
