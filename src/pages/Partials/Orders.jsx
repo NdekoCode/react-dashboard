204199953f8e0a9385a66190f4dcde9b27b2c0fa
NdekoCode <arickbulakali@gmail.com>
2023-02-18 17:38:53 +0200

    feat: display orders data

--- src/pages/Partials/Orders.jsx
+++ src/pages/Partials/Orders.jsx
@@ -1,10 +1,44 @@
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  ContextMenu,
+  Edit,
+  ExcelExport,
+  Filter,
+  GridComponent,
+  Inject,
+  Page,
+  PdfExport,
+  Resize,
+  Sort,
+} from "@syncfusion/ej2-react-grids";
 import React from "react";
 import { Header } from "../../components/components";
+import { ordersData, ordersGrid } from "../../libs/data/dummy";
 
 const Orders = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
+      <GridComponent id="gridcomp" dataSource={ordersData} allowPaging>
+        <ColumnsDirective>
+          {ordersGrid.map((order, index) => (
+            <ColumnDirective key={index} {...order} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Page,
+            Sort,
+            Filter,
+            ContextMenu,
+            ExcelExport,
+            PdfExport,
+            Edit,
+          ]}
+        />
+      </GridComponent>
     </div>
   );
 };
