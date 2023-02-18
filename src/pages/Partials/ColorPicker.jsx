d12a5918cccde38ab1e24bf12c03fc197cb888d3
NdekoCode <arickbulakali@gmail.com>
2023-02-18 22:56:14 +0200

    feat: add static color palette components

--- src/pages/Partials/ColorPicker.jsx
+++ src/pages/Partials/ColorPicker.jsx
@@ -1,7 +1,37 @@
+import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
 import React from "react";
-
+import { Header } from "../../components/components";
 function ColorPicker() {
-  return <div className="FadeAp">ColorPicker</div>;
+  return (
+    <div className="FadeAp m-2 p-2 md:m-10 md:p-10 rounded-3xl bg-white">
+      <Header category="App" title="Color Picker" />
+      <div className="text-center">
+        <div id="preview" />
+        <div className="flex justify-center items-center gap-20 flex-wrap">
+          <div>
+            <h4 className="text-2xl font-semibold mb-4 mt-2">Inline Palette</h4>
+            <ColorPickerComponent
+              id="inline-palette"
+              mode="Palette"
+              modeSwitcher={false}
+              inline
+              showButtons={false}
+            />
+          </div>
+          <div>
+            <h4 className="text-2xl font-semibold mb-4 mt-2">Inline Picker</h4>
+            <ColorPickerComponent
+              id="inline-picker"
+              mode="Picker"
+              modeSwitcher={false}
+              inline
+              showButtons={false}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ColorPicker;
--- src/pages/Partials/Editor.jsx
+++ src/pages/Partials/Editor.jsx
@@ -9,12 +9,14 @@ import {
 } from "@syncfusion/ej2-react-richtexteditor";
 import React from "react";
 import { Header } from "../../components/components";
+import { EditorData } from "../../libs/data/dummy";
 
 function Editor() {
   return (
     <div className="FadeAp m-2 p-2 md:m-10 md:p-10 rounded-3xl bg-white">
       <Header category="App" title="Editor" />
       <RichTextEditorComponent>
+        <EditorData />
         <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
       </RichTextEditorComponent>
     </div>
