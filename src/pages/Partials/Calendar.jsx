3b05a00f2f1e8dae2c2e9cb62e2e9d24a2f19345
NdekoCode <arickbulakali@gmail.com>
2023-02-18 19:43:39 +0200

    feat: add basics calendar view

--- src/pages/Partials/Calendar.jsx
+++ src/pages/Partials/Calendar.jsx
@@ -1,7 +1,25 @@
+import {
+  Agenda,
+  Day,
+  DragAndDrop,
+  Inject,
+  Month,
+  Resize,
+  ScheduleComponent,
+  Week,
+  WorkWeek,
+} from "@syncfusion/ej2-react-schedule";
 import React from "react";
-
 function Calendar() {
-  return <div>Calendar</div>;
+  return (
+    <div className="m-2 p-2 md:m-10 md:p-10 mt-20 bg-white rounded-3xl">
+      <ScheduleComponent>
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
+    </div>
+  );
 }
 
 export default Calendar;
