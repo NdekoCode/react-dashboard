e1290da9b97b8b7756dd9b500410ee9e3e4b7025
NdekoCode <arickbulakali@gmail.com>
2023-02-19 19:00:48 +0200

    style: add button setting close

--- src/components/Partials/ThemeSettings.jsx
+++ src/components/Partials/ThemeSettings.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { MdOutlineCancel } from "react-icons/md";
 
-const ThemeSettings = () => <div>ThemeSettings</div>;
+const ThemeSettings = () => {
+  return (
+    <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
+      <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400">
+        <div className="flex items-center justify-between p-4 ml-4">
+          <h4 className="font-bold text-lg">Settings</h4>
+          <button
+            type="button"
+            className="text-2xl rounded-full p-2 hover:drop-shadow-xl hover:bg-light-gray"
+            onClick={() => {}}
+            style={{ color: "rgba(153,171,180)" }}
+          >
+            <MdOutlineCancel />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ThemeSettings;
