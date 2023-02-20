b10a400d5b76e2c68eeb2fc9fd104e22887302b1
NdekoCode <arickbulakali@gmail.com>
2023-02-20 15:20:24 +0200

    feat: add lineChart component

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Header, LineChart } from "../../components/components";
 
 function Line() {
-  return <div className="FadeAp">Line</div>;
+  return (
+    <div className="FadeAp m-4 mt-34 dark:bg-secondary-dark-bg dark:text-gray-400 md:m-10 p-10 rounded-3xl bg-white">
+      <Header category="Chart" title="Inflation Rate" />
+      <div className="w-full">
+        <LineChart />
+      </div>
+    </div>
+  );
 }
 
 export default Line;
