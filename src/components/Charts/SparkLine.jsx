958f6047203d97c05167c50033e4d2c293583357
NdekoCode <arickbulakali@gmail.com>
2023-02-20 15:44:01 +0200

    refactor: Sparkline component to class Component

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -3,35 +3,38 @@ import {
   SparklineComponent,
   SparklineTooltip,
 } from "@syncfusion/ej2-react-charts";
-import React from "react";
-
-const SparkLine = ({ currentColor, id, type, height, width, color, data }) => {
-  return (
-    <div>
-      <SparklineComponent
-        id={id}
-        height={height}
-        width={width}
-        lineWidth={1}
-        valueType="Numeric"
-        fill={color}
-        border={{ color: currentColor, width: 2 }}
-        dataSource={data}
-        xName="x"
-        yName="yval"
-        type={type}
-        tooltipSettings={{
-          visible: true,
-          format: "${x} data ${yval}",
-          trackLineSettings: {
+import React, { Component } from "react";
+export default class SparkLine extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { currentColor, id, type, height, width, color, data } = this.props;
+    return (
+      <div>
+        <SparklineComponent
+          id={id}
+          height={height}
+          width={width}
+          lineWidth={1}
+          valueType="Numeric"
+          fill={color}
+          border={{ color: currentColor, width: 2 }}
+          dataSource={data}
+          xName="x"
+          yName="yval"
+          type={type}
+          tooltipSettings={{
             visible: true,
-          },
-        }}
-      >
-        <Inject services={[SparklineTooltip]} />
-      </SparklineComponent>
-    </div>
-  );
-};
-
-export default SparkLine;
+            format: "${x} data ${yval}",
+            trackLineSettings: {
+              visible: true,
+            },
+          }}
+        >
+          <Inject services={[SparklineTooltip]} />
+        </SparklineComponent>
+      </div>
+    );
+  }
+}
--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -21,9 +21,9 @@ const Area = () => {
   const { currentMode } = StateContext();
   return (
     <div className="FadeAp m-4 mt-34 dark:bg-secondary-dark-bg dark:text-gray-400 md:m-10 p-10 rounded-3xl bg-white">
-      <Header category="Chart" title="Inflation Rate" />
+      <Header category="Chart" title="Inflation Rate in Percentage    " />
       <ChartComponent
-        id="line-chart"
+        id="area-chart"
         height="450px"
         primaryXAxis={areaPrimaryXAxis}
         primaryYAxis={areaPrimaryYAxis}
